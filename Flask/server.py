f475e011f80c19c784eccd7a2110f689908f6fe7
Akinaru <90217410+Akinaru@users.noreply.github.com>
2022-03-25 23:57:55 +0100

    Add files via upload

--- Flask/server.py
+++ Flask/server.py
@@ -1,1 +1,33 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+
+@app.route('/')
+def principal():
+    return render_template("on.html")
+
+
+
+
+@app.route('/on/')
+def on():
+
+  print ('La led est ON')
+  return render_template("on.html")
+
+
+
+
+
+
+@app.route('/off/')
+def off():
+
+  print ('La led est OFF')
+  return render_template("off.html")
+
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
